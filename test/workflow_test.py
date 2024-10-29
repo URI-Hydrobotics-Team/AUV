63fb8aa7226c77fa17441f985ba8587a1e295c0d
Hydro Deckbox <urihydrobotics@gmail.com>
2024-10-28 20:38:04 -0400

    For whatever reason this ms5837 uses a very specific arg parser/logger. Which of course doesn't work in python3.

--- test/workflow_test.py
+++ test/workflow_test.py
@@ -14,7 +14,6 @@ class AClass:
         Parameters:
             value (int or float): The initial value to store in the instance.
         """
-        __test__ = False
         #Stupid requirement because it's stupid and a requirement if the word Test is at the start of your class.
         self.value = value
 
