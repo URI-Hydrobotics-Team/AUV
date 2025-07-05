60983e73e987466608397d6ccc9cd5d8a9b78943
roach350 <dome.c4129@gmail.com>
2025-07-05 12:25:58 -0400

    pressure sensor changes

--- sensors/pressure-sensor/main.cpp
+++ sensors/pressure-sensor/main.cpp
@@ -1,10 +1,27 @@
-/* test code for MS5837 driver */
+
+#include <iostream>
 #include "driver.h"
+
+#define MS5837_ADDR 0x76
+
+void test(){
+
+	MS5837 sensor;
+	sensor.init(MS5837_ADDR);
+
+	while(1){
+		std::cout << sensor.getPressure() << " mbar\n";
+		std::cout << sensor.getTemperature() << " deg C\n";
+		std::cout << sensor.getDepth() << " m\n";
+		std::cout << sensor.getAltitude() << " m above mean sea level\n";
+
+	}
+}
 
 
 int main(){
 
-
+	test();
 
 	return 0;
 }
