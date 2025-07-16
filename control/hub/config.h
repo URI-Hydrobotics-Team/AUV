da1a9fdacbd506f7f1af65ea2e1cb937c8534930
roach350 <dome.c4129@gmail.com>
2025-07-16 14:23:58 -0400

    heavily improved socket class

--- control/hub/config.h
+++ control/hub/config.h
@@ -4,7 +4,9 @@
 /* CONNECTIONS */
 #define DECKBOX_PORT_TX 8100
 #define DECKBOX_PORT_RX 8200
-#define DECKBOX_IP "127.0.0.1"
+#define DECKBOX_IP "172.20.132.67"
+//#define DECKBOX_IP "127.0.0.1"
+#define LOG_IP "127.0.0.1"
 #define LOG_PORT_TX 8101
 #define PICO_SERIAL_PORT "/dev/ttyACM0"
 
